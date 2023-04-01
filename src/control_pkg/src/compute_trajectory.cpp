ffaae83fa420b2182400a2f56bc55bf53d58741c
JavierFR <javierfelixrendon@gmail.com>
2023-03-31 19:07:09 -0600

    update aruco detector and camera matrix

--- src/control_pkg/src/compute_trajectory.cpp
+++ src/control_pkg/src/compute_trajectory.cpp
@@ -21,4 +21,57 @@
 #include <geometry_msgs/msg/pose.hpp>
 #include <interfaces/msg/positions.hpp>
 
+using std::placeholders::_1;
+using namespace std::chrono_literals;
+
 geometry_msgs::msg::Pose Robot1, Robot2, Object1, Object2, Target;
+
+float x_grid = 0.10;  //All dimensions in meters
+float y_grid = 0.10;
+float x_world = 3.1;            //Set a number that is odd after dividing it by x_grid.
+float y_world = 3.1;            //Set a number that is ood after dividing it by y_grid.
+
+int n_x_spaces = (int)x_world/x_grid;
+float n_y_spaces = y_world/y_grid;
+
+class Compute_Trajectory : public rclcpp::Node
+{
+    public:
+        Compute_Trajectory() : Node("compute_trajectory")
+        {
+            subs_position = this->create_subscription<interfaces::msg::Positions>(
+                "/positions", 1, std::bind(&Compute_Trajectory::subs_callback,this,_1));
+
+            std::cout<<n_x_spaces<< std::endl;
+        }
+    
+
+    private:
+
+
+        void subs_callback(const interfaces::msg::Positions::SharedPtr pos_msg)
+        {
+            Robot1=pos_msg->pos_robot1;
+            Robot2=pos_msg->pos_robot2;
+            Object1=pos_msg->pos_object1;
+            Object2=pos_msg->pos_object2;
+            Target=pos_msg->pos_target;
+
+        }
+
+
+
+
+
+
+
+    rclcpp::Subscription<interfaces::msg::Positions>::SharedPtr subs_position;
+};
+
+int main(int argc, char * argv[])
+{
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<Compute_Trajectory>());
+    rclcpp::shutdown();
+    return 0;
+}
