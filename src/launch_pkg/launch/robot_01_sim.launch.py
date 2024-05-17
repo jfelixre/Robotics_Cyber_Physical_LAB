334e052c31e83ab546f82203513debf1243d6669
jfelixre <javierfelixrendon@gmail.com>
2024-05-16 19:19:57 -0600

    Create launch file for robot_01 sim

---
+++ src/launch_pkg/launch/robot_01_sim.launch.py
@@ -0,0 +1,78 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.actions import ExecuteProcess
+from launch.conditions import IfCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    # Configure ROS nodes for launch
+
+    # Setup project paths
+    pkg_project_control_pkg = get_package_share_directory('control_pkg')
+    pkg_project_gazebo_plugin_sim = get_package_share_directory('gazebo_plugin_sim')
+    pkg_project_img_proc_pkg = get_package_share_directory('img_proc_pkg')
+    pkg_project_interfaces = get_package_share_directory('interfaces')
+    pkg_project_inv_kinematics_pkg = get_package_share_directory('inv_kinematics_pkg')
+    pkg_project_launch_pkg = get_package_share_directory('launch_pkg')
+    pkg_project_robot_custom_description = get_package_share_directory('robot_custom_description')
+    pkg_ros_gz_sim = get_package_share_directory('ros_gz_sim')
+
+    #Load yaml parameters
+    config = os.path.join(
+    pkg_project_launch_pkg,
+    'config',
+    'params.yaml'
+    )
+
+    # Spawn robot in Gazebo
+    file = os.path.join(
+        pkg_project_robot_custom_description,
+        'models',
+        'robot_01',
+        'model.sdf'
+    )
+    #Command to spawn robot in Gazebo on an especific position
+    gz_robot_spawn = ExecuteProcess(
+        cmd=[[
+            'ros2 run ros_gz_sim create --args -file "',
+            file,
+            '" -name robot_01 -x -1 -y 1 -z 0.09'
+
+        ]],
+        shell=True
+    )
+
+
+    # Bridge ROS topics and Gazebo messages for establishing communication
+    bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        parameters=[{
+            'config_file': os.path.join(pkg_project_launch_pkg, 'config', 'bridge_r01.yaml'),
+            'qos_overrides./tf_static.publisher.durability': 'transient_local',
+        }],
+        output='screen'
+    )
+
+    # #Unpause simulation
+    # bridge_unpause = ExecuteProcess(
+    #     cmd=[[
+    #         'ros2 run ros_gz_bridge parameter_bridge /world/world_cam/control@ros_gz_interfaces/srv/ControlWorld'
+    #     ]],
+    #     shell=True
+    # )
+
+    return LaunchDescription([
+        gz_robot_spawn,
+        bridge,
+        #bridge_unpause,
+        
+    ])
